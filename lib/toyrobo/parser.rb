7cbd975461bfc632297f9da2d8c11250312c6cf5
Carl Tablante <carlwilliam.tablante@gmail.com>
2023-09-08 14:31:46 +1000

    Improving the grammar of the parser

--- lib/toyrobo/parser.rb
+++ lib/toyrobo/parser.rb
@@ -5,6 +5,11 @@ require_relative "ast/num"
 require_relative "ast/string"
 require_relative "ast/command"
 
+# Grammar
+# CMD is Terminal
+# E -> CMD | CMD {ARGS}*
+# ARGS -> NUM | STRING
+
 module Toyrobo
   # The Parser takes a sequence of tokens and produces a data structure that can be understood by the interpreter.
   # The data structure is usually an AST but given that the problem is quite simple, it's a list
@@ -15,34 +20,24 @@ module Toyrobo
       @nodes = []
     end
 
+    # Grammar:
+    # E -> CMD | CMD ARGS
     def run
-      while pending_tokens?
-        t = @tokens[0]
+      token = consume
+      return [] if token.nil? || token.type != :command
 
-        next if t.type != :command
+      node = AST::Command.new(token.text, token.type)
+      node.params = args
 
-        node = AST::Command.new(t.text, t.type)
-        consume
-        parse_arguments(node)
-      end
-
-      @nodes
+      @nodes << node
     end
 
-    def pending_tokens?
-      !@tokens.empty?
-    end
+    def args
+      return [] if @tokens[0].nil?
 
-    def parse_arguments(command_node)
-      while @tokens[0] && @tokens[0].type != :command
-        t = @tokens[0]
-
-        param_node = t.type == :num ? AST::Num.new(t.text, t.type) : Toyrobo::AST::String.new(t.text, t.type)
-        command_node.params << param_node
-        consume
+      @tokens.map do |t|
+        t.type == :num ? AST::Num.new(t.text, t.type) : Toyrobo::AST::String.new(t.text, t.type)
       end
-
-      @nodes << command_node
     end
 
     def consume
